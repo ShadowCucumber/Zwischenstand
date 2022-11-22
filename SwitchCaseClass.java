62c6f15e321cd4469d1c014b15ccff22b1b61633
Yusuf347 <yusuf.cheik.mostafa@gymhum.de>
2022-11-22 09:12:27 +0100

    Create SwitchCaseClass.java

---
+++ SwitchCaseClass.java
@@ -0,0 +1,24 @@
+public class SwitchCaseClass {
+    public static void main(String[]args){
+        int i=2;
+
+        switch(i){
+            case 0:
+            System.out.println("I ist null");
+            break;
+            case 1:
+            System.out.println("I ist eins");   
+            break;
+            case 2:
+            System.out.println("I ist zwei");
+            break;
+            case 3:
+            System.out.println("I ist drei");
+            break;
+            default:
+            System.out.println("I liegt nicht zwicshen null und drei");
+            break;
+        }
+    }
+    
+}
