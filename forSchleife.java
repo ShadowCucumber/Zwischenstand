fff9c0e28f9de6d2bb3d6dc75212d603a6a7050b
Yusuf347 <yusuf.cheik.mostafa@gymhum.de>
2022-11-22 09:29:04 +0100

    Create forSchleife.java

---
+++ forSchleife.java
@@ -0,0 +1,13 @@
+public class forSchleife {
+    
+    public void gibAus(){
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+    
+    public static void main(String[] args){
+        forSchleife fc = new forSchleife();
+        fc.gibAus();
+    }
+}
